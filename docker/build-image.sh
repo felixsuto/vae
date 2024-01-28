650f896cd043b330e859eeb80cb1cfe21608a5d1
felixsuto <felix.suto7@gmail.com>
2024-01-28 19:10:21 +0100

    .

--- docker/build-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker build --no-cache \
---build-arg USRNM=$(whoami) \
---build-arg USRUID=$(id -u) \
---build-arg USRGID=$(id -g) \
--t vae:latest .
--- run.sh
+++
@@ -1,3 +0,0 @@
-docker run -p 8888:8888 \
-    -v $HOME/felix/vae:/host --gpus all \
-    --rm -it vae:latest 
